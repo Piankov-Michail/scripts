598865b8f7278f89e2934b0c380cefc5e91baba3
Piankov-Michail <mishan.rus2014@gmail.com>
2023-10-14 12:21:08 +0300

    scrtipt.sh added

---
+++ script.sh
@@ -0,0 +1,23 @@
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+sudo snap install telegram-desktop
+
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get install google-chrome-stable
+
+sudo snap install discord
+
+sudo snap install pycharm-community --classic
+
+sudo snap install --classic code
+
+sudo snap install libreoffice
+
+sudo snap install zoom-client
+
+
+
+
+
