bd7b937328687a6eab2a7a8d91370ed153d24701
Piankov-Michail <mishan.rus2014@gmail.com>
2023-10-14 13:17:39 +0300

    mod script.sh

--- script.sh
+++ script.sh
@@ -2,20 +2,27 @@ sudo apt-get update
 sudo apt-get dist-upgrade
 
 sudo snap install telegram-desktop
+sudo apt-get update
 
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
 sudo apt-get install google-chrome-stable
 
 sudo snap install discord
+sudo apt-get update
 
 sudo snap install pycharm-community --classic
+sudo apt-get update
 
 sudo snap install --classic code
+sudo apt-get update
 
 sudo snap install libreoffice
+sudo apt-get update
 
 sudo snap install zoom-client
+sudo apt-get update
 
 
 
